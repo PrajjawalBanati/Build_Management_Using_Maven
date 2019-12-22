be66db11e40d357ce5587cef6073b733c5c02d1d
500060722 <500060722@stu.upes.ac.in>
2019-12-22 14:25:41 +0530

    Added Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,1 +1,54 @@
+package calculator;
+class Multiplication{
+	public int e;
+	public double f;
+void mul(double a,double b)//both types are double
+{
+	f=a*b;
+	System.out.println(f);
+}
+void mul(int a,int b)//both types are int
+{
+	e=a*b;
+	System.out.println(e);
+}
+void mul(int a,double b)//one type is int and one is double
+{
+	f=a*b;
+	System.out.println(f);
+}
+}
+class Addition extends Multiplication{
+	public int c;
+	public double d;
+void add(double a,double b)//both types are double
+{
+	d=a+b;
+	System.out.println(d);
+}
+void add(int a,int b)//both types are int
+{
+	c=a+b;
+	System.out.println(c);
+}
+void add(int a,double b)//one type is int and one is double
+{
+	d=a+b;
+	System.out.println(d);
+}
+}
+class Calculator extends Addition{
+	public static void main(String[]args)
+	{
+		Calculator obj = new Calculator();
+		obj.add(3,2);
+		obj.add(3.1f,4);
+		obj.add(3.1f,8.1f);
+		obj.mul(3,2);
+		obj.mul(3.1f,4);
+		obj.mul(3.1f,8.1f);
+	}
+}
+
+	
 
